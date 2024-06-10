96d0df3cbdf1c769d31bcc3daf85970c4e5eef4c
F-O-P <tayler.coon@gmail.com>
2024-06-10 11:01:26 -0500

    added new line

--- 0x00-hello-dart/0-hello_holberton.dart
+++ 0x00-hello-dart/0-hello_holberton.dart
@@ -1,5 +1,3 @@
-// Write a dart program that prints Hello Holberton! followed by a new line using the function print.
-
 void main() {
-  print('Hello Holberton!');
+  print('Hello Holberton!\n');
 }
