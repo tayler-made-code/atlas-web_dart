7beb2aa360c4a70f6167215575b96e9c8e8f35c4
F-O-P <tayler.coon@gmail.com>
2024-06-19 08:48:10 -0500

    task 1 completed

---
+++ 0x02-dart-classes/1-usertojson.dart
@@ -0,0 +1,13 @@
+class User {  
+  // properties for user
+  String name;
+  int age;
+  double height;
+
+  User({required this.name, required this.age, required this.height});
+
+  // method to return user as json
+  String toJson() {
+    return '{"name": "$name", "age": $age, "height": $height}';
+  }
+}
