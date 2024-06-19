dadaa0b7ee2f7b0b8f43dd81d9284fb0c181093c
F-O-P <tayler.coon@gmail.com>
2024-06-19 10:14:22 -0500

    task 3 completed

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,19 @@
+class Password {  
+  // private property for password
+  String _password;
+
+  Password({String password = ''}) : _password = password;
+
+  // method to check if password is valid
+  bool isValid() {
+    if (_password.length < 8 || _password.length > 16)
+      return false;
+    if (!_password.contains(RegExp(r'[A-Z]')) || !_password.contains(RegExp(r'[a-z]')) || !_password.contains(RegExp(r'[0-9]')))
+      return false;
+    return true;
+  }
+
+  String toString() {
+    return 'Your Password is $_password';
+  }
+}
