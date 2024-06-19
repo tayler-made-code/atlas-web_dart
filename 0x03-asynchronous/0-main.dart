a9d721bda5bdf81c2aa2b50e11ae890e9c901ab6
F-O-P <tayler.coon@gmail.com>
2024-06-19 18:03:28 -0500

    fixing main file

--- 0x03-asynchronous/0-main.dart
+++ 0x03-asynchronous/0-main.dart
@@ -3,9 +3,3 @@ import '0-users_count.dart';
 main() async {
   await usersCount();
 }
-achref@achref:~$ cat 0-util.dart
-
-Future<int> fetchUsersCount() => Future.delayed(
-      const Duration(seconds: 2),
-      () => 19,
-    );
