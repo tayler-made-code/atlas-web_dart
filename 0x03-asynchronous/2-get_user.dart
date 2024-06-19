3647d449153db923ca1dea5f99a9921aa9355caf
F-O-P <tayler.coon@gmail.com>
2024-06-19 18:33:55 -0500

    task 2 copleted

---
+++ 0x03-asynchronous/2-get_user.dart
@@ -0,0 +1,12 @@
+import '2-util.dart';
+import 'dart:convert';
+
+Future<void> getUser() async {
+  try {
+    String userData = await fetchUser();
+    Map<String, dynamic> user = json.decode(userData);
+    print(user);
+  } catch (e) {
+    print(e);
+  }
+}
