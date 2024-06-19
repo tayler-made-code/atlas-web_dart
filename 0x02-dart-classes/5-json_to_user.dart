24b52915392068fdfd47aa0fd4b40166c644822a
F-O-P <tayler.coon@gmail.com>
2024-06-19 10:30:09 -0500

    task 5 completed

---
+++ 0x02-dart-classes/5-json_to_user.dart
@@ -0,0 +1,23 @@
+class User {  
+  // properties for user
+  String name;
+  int age;
+  double height;
+  int id;
+
+  User({required this.id, required this.name, required this.age, required this.height});
+
+  // method to return user as json
+  String toJson() {
+    return '{id : $id, name: $name, age: $age, height: $height}';
+  }
+
+  // method to create user from json
+  static User fromJson(Map<dynamic, dynamic> userJson) {
+    return User(id: userJson['id'], name: userJson['name'], age: userJson['age'], height: userJson['height']);
+  }
+
+  String toString() {
+    return 'User(id: $id, name: $name, age: $age, height: $height)';
+  }
+}
