4f9cf969196007b6fbd41a19854f1ba36cb4ab5c
F-O-P <tayler.coon@gmail.com>
2024-06-20 09:35:20 -0500

    moved all code inside of try block

--- 0x03-asynchronous/rick_and_morty.dart
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -2,25 +2,26 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<void> printRmCharacters() async {
-  final url = 'https://rickandmortyapi.com/api/character';
-
   try {
+    final url = 'https://rickandmortyapi.com/api/character';
     final response = await http.get(Uri.parse(url));
 
     if (response.statusCode == 200) {
       final data = json.decode(response.body);
       final count = data['info']['count'];
+      print('count: $count');
+      print('info: ${data['info']}');
 
       final characterIds = List.generate(count, (index) => index + 1);
       final characterIdsString = characterIds.join(',');
 
-      final updatedUrl = 'https://rickandmortyapi.com/api/character/$characterIdsString';
-      //test the url
-      // print('$updatedUrl');
-      final updatedResponse = await http.get(Uri.parse(updatedUrl));
+      final fullUrl = 'https://rickandmortyapi.com/api/character/$characterIdsString';
+      // test the url
+      // print('$fullUrl');
+      final fullResponse = await http.get(Uri.parse(fullUrl));
 
-      if (updatedResponse.statusCode == 200) {
-        final updatedData = json.decode(updatedResponse.body);
+      if (fullResponse.statusCode == 200) {
+        final updatedData = json.decode(fullResponse.body);
 
         for (var character in updatedData) {
           print(character['name']);
