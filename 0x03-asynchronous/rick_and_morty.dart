6bb0ac941cf7fa58b945c62b4f6015d9893f6f9a
F-O-P <tayler.coon@gmail.com>
2024-06-19 20:51:12 -0500

    task 5 completed

---
+++ 0x03-asynchronous/rick_and_morty.dart
@@ -0,0 +1,37 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+Future<void> printRmCharacters() async {
+  final url = 'https://rickandmortyapi.com/api/character';
+
+  try {
+    final response = await http.get(Uri.parse(url));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final results = data['results'];
+
+      for (var character in results) {
+        print(character['name']);
+      }
+
+      while (data['info']['next'] != null) {
+        final nextUrl = data['info']['next'];
+        final nextResponse = await http.get(Uri.parse(nextUrl));
+
+        if (nextResponse.statusCode == 200) {
+          final nextData = json.decode(nextResponse.body);
+          final nextResults = nextData['results'];
+
+          for (var character in nextResults) {
+            print(character['name']);
+          }
+
+          data['info']['next'] = nextData['info']['next'];
+        }
+      }
+    }
+  } catch (error) {
+    print('error caught: $error');
+  }
+}
